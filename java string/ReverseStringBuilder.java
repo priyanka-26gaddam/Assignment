e02f737741ac1da204acfde9917c379d959b1ed9
priyanka-26gaddam <91132282+priyanka-26gaddam@users.noreply.github.com>
2022-01-14 00:28:30 +0530

    Add files via upload

---
+++ java string/ReverseStringBuilder.java
@@ -0,0 +1,12 @@
+
+public class ReverseStringBuilder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  StringBuilder sb= new StringBuilder("This method returns the reversed object on which it was called");
+	       sb.reverse();
+	       System.out.println(sb);
+
+	}
+
+}
