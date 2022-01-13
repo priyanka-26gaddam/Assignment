30781859499e02661d25757460d5a7a1e38f8f30
priyanka-26gaddam <91132282+priyanka-26gaddam@users.noreply.github.com>
2022-01-14 00:27:25 +0530

    Add files via upload

---
+++ java string/ReplaceString.java
@@ -0,0 +1,12 @@
+
+public class ReplaceString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer sb=new StringBuffer("It is used to at the specified index position");
+		sb.insert(14, "insert text ");
+		System.out.println(sb);
+
+	}
+
+}
