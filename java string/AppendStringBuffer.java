26191b327235eb2adc08b36c965a17b8370437ac
priyanka-26gaddam <91132282+priyanka-26gaddam@users.noreply.github.com>
2022-01-14 00:26:10 +0530

    Add files via upload

---
+++ java string/AppendStringBuffer.java
@@ -0,0 +1,13 @@
+
+public class AppendStringBuffer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer sb=new StringBuffer("StringBuffer");
+		  sb.append(" is a peer class of String");
+		   sb.append(" that provides much of"); 
+           sb.append(" the functionality of the string");
+           System.out.println(sb);
+	}
+
+}
