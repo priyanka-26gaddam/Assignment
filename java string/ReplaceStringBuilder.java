1e0ea4e5b45c0058756dcc754ba36cc1a055b9ca
priyanka-26gaddam <91132282+priyanka-26gaddam@users.noreply.github.com>
2022-01-14 00:27:45 +0530

    Add files via upload

---
+++ java string/ReplaceStringBuilder.java
@@ -0,0 +1,13 @@
+
+public class ReplaceStringBuilder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuilder sb=new StringBuilder("It is used to at the specified index position");
+		sb.insert(14, "insert text ");
+		System.out.println(sb);
+
+
+	}
+
+}
