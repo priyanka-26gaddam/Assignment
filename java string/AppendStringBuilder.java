81f0956c057343cf501dd83941b762ad415e2d09
priyanka-26gaddam <91132282+priyanka-26gaddam@users.noreply.github.com>
2022-01-14 00:26:35 +0530

    Add files via upload

---
+++ java string/AppendStringBuilder.java
@@ -0,0 +1,14 @@
+
+public class AppendStringBuilder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuilder sb=new StringBuilder("StringBuilder");
+		  sb.append(" is a peer class of String");
+		   sb.append(" that provides much of"); 
+         sb.append(" the functionality of the string");
+         System.out.println(sb);
+
+	}
+
+}
