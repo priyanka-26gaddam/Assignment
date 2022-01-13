f6f1cd79da165cfc6bbb50536a93953a378731ee
priyanka-26gaddam <91132282+priyanka-26gaddam@users.noreply.github.com>
2022-01-14 00:28:12 +0530

    Add files via upload

---
+++ java string/ReverseString.java
@@ -0,0 +1,11 @@
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       StringBuffer sb= new StringBuffer("This method returns the reversed object on which it was called");
+       sb.reverse();
+       System.out.println(sb);
+	}
+
+}
